b649c09c0bf56d49eaefb9e0fd0decc8ba5052de
DanielCastañeda <pipecastov21@hotmail.com>
2026-07-15 16:06:29 -0500

    feat: add Todo DTOs

---
+++ ToDoAPI.Application/DTOs/TodoDto.cs
@@ -0,0 +1,5 @@
+﻿namespace ToDoAPI.Application.DTOs;
+
+public record CreateTodoDto(string Title);//lo que el cliente manda en el POST. Solo el título.
+public record UpdateTodoDto(string Title, bool IsCompleted);// lo que el cliente manda en el PUT. Título y si está completada.
+public record TodoResponseDto(int Id, string Title, bool IsCompleted, DateTime CreatedAt);//lo que el API devuelve al cliente. Todo: Id, título, estado y fecha.
